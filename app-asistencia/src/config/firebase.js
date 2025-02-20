f525b3adf2332b8a26cf0dfa4cc3bbc30c169ecd
Nicolas D. Rodriguez <ndr126@abc.gob.ar>
2025-02-20 20:22:36 -0300

    refactor: move Firebase config to environment variables

--- app-asistencia/src/config/firebase.js
+++ app-asistencia/src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmLxOk9lD1IPvGffm19B5SvRwAUWrfQhQ",
-  authDomain: "nicolas-d-rodriguez.firebaseapp.com",
-  projectId: "nicolas-d-rodriguez",
-  storageBucket: "nicolas-d-rodriguez.firebasestorage.app",
-  messagingSenderId: "880070527779",
-  appId: "1:880070527779:web:4e00303e8bd0b8cfd7da96",
-  measurementId: "G-B0DVXW03BC"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
