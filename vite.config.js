9c5a31c400ce02734a43261457dbb0ccba29574a
Nicolas D. Rodriguez <ndr126@abc.gob.ar>
2025-02-24 14:16:55 -0300

    Implementacion de PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Assistance System',
+        short_name: 'AS',
+        description: 'Sistema de Control de Asistencia',
+        theme_color: '#4F46E5',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/AS-Logo-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/AS-Logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: '/app-asistencia/',
   server: {
     port: 3000,
